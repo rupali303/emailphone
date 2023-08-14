b94662cab0074cc30102ce408e93b09d3564588d
rupali patil <rupalishewale2010@gmail.com>
2023-08-15 02:08:46 +0530

    work on it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Task5New from './Task-5-new/Main';
 import TaskTwo from "./TaskTwo/Main"
+import TaskThree from "./TaskThree/Main"
 
 function App() {
   return (
     <div>
     {/* <Task5New/> */}
-    <TaskTwo/>
+    {/* <TaskTwo/> */}
+    <TaskThree/>
     </div>
   );
 }
